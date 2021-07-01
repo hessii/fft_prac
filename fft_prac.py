07fe86a7b42729049f7a68d35f772214ebb32bd9
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-01 17:46:27 +0900

    changed omega, freqs, fft_theo

--- fft_prac.py
+++ fft_prac.py
@@ -1,23 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy.fft import fft, fftfreq, ifft
 # numpy.fft.fft: compute the one-dimensional discrete Fourier transform
 # numpy.fft.fftfreq: return the discrete Fourier Transform sample frequencies
 # numpy.fft.ifft: compute the one-dimensional inverse discrete Fourier transform
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Setup for domain
 # number of points
 n = 1000
 
 # Distance (in meters) or time period (in seconds)
-Lx = 100        # period = 100
+Lx = 100        # total period = 100 (total length of the graph)
 
 # angular frequency omega
 omg = 2.0 * np.pi / Lx
 
 # Creating individual signals
-x = np.linspace(0, Lx, n)
-y1 = 1. * np.cos(5. * omg * x)      # signal 1
+x = np.linspace(0, Lx, n)       # [0, 0.1001001, 0,2002002, ..., 100]
+y1 = 1. * np.cos(5. * omg * x)      # signal 1, angular velocity = 5w (5 times the angular velocity omg)
 y2 = 2. * np.sin(10. * omg * x)     # signal 2
 y3 = 0.5 * np.sin(20. * omg * x)    # signal 3
 # (numpy.linspace(start, stop, num=50): #return evenly spaced numbers over a specified interval)
@@ -27,7 +27,9 @@ y = y1 + y2 + y3
 
 # Preparatory steps
 # Create all the necessary frequencies
-freqs = np.fft.fftfreq(n)      # [0, 0.001, 0.002, 0.003, ..., 0.049]
+#fft.fftfreq(n, d=1.0): DFT sample frequencies, n: window length, d: sample spacing(default=1)
+dk = 2 * np.pi / Lx     # frequency spacing
+freqs = np.fft.fftfreq(int(dk*n), dk)      # [0,0.01, 0.02, ..., 4.99]
 
 # make array to be used for power spectra
 # ignoring half the values, as they are complex conjugates of the other
@@ -40,7 +42,7 @@ fft_vals = np.fft.fft(y)
 
 # true theoretical fft
 # 2*|| for ..?
-fft_theo = 2.0 * np.abs(fft_vals/n)
+fft_theo = 2.0 * np.abs(fft_vals) / n
 
 plt.figure(1)
 plt.title('Original Signal')
