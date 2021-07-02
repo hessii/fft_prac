1209b8017d4bdde514610a92d7a6277a2ac2d43e
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-02 13:27:50 +0900

    changes in the index slicing, fft code, parseval codes

--- fft_sin.py
+++ fft_sin.py
@@ -2,30 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # sine graph
-n = 100
+n = 19
 L = 2 * np.pi
-dx = L/(n-1)
-x = np.linspace(0, L, n)
-y = np.sin(x)
+dx = L/n
+x = np.linspace(0, L, n + 1)
+x = x[:n]
+y = 10 * np.sin(1*x)
 
 fig1 = plt.figure()
 plt.plot(x, y, '.-')
 plt.show()
 
 # FFT
-fft = np.fft.fft(y[:n-1])
+fft = np.fft.fft(y)
+fft = np.fft.fftshift(fft)
+fft_mag = 2 * np.abs(fft) / n
+
 dk = 2 * np.pi / L
-kmax = dk * (n-1)
-k = np.linspace(0, kmax, n)
-xfft = k[:n-1]
+k = np.arange(-np.floor(n/2), n/2, 1)*dk
+xfft = k
 
 fig2 = plt.figure()
-plt.plot(xfft, fft)
+plt.plot(xfft[int(n/2):], fft_mag[int(n/2):], '.-')
 
+#Parseval's theorem
+fsqravg = np.mean(np.abs(y) ** 2)
 
-fsqr = np.abs(y[:n-1]) ** 2
-fsqravg = (1/(n-1)) * sum(fsqr)
+csqrsum = sum(np. abs(fft) ** 2)
 
-csqr = np.abs(fft[:n-1]) ** 2
-csqrsum = sum(csqr)
+print(fsqravg, ', ', csqrsum/(n*n))
 
