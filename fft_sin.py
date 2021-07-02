903918fc06e7fae0c210fad8557c82e903206d06
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-02 15:06:30 +0900

    k: np.arange -> fft.fftfreq

--- fft_sin.py
+++ fft_sin.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # sine graph
-n = 19
+n = 101
 L = 2 * np.pi
 dx = L/n
 x = np.linspace(0, L, n + 1)
@@ -11,19 +11,25 @@ y = 10 * np.sin(1*x)
 
 fig1 = plt.figure()
 plt.plot(x, y, '.-')
-plt.show()
+plt.title('sine function')
 
 # FFT
 fft = np.fft.fft(y)
 fft = np.fft.fftshift(fft)
 fft_mag = 2 * np.abs(fft) / n
 
+'''
 dk = 2 * np.pi / L
 k = np.arange(-np.floor(n/2), n/2, 1)*dk
 xfft = k
+'''
+
+k = np.fft.fftshift(np.fft.fftfreq(n, dx))
 
 fig2 = plt.figure()
-plt.plot(xfft[int(n/2):], fft_mag[int(n/2):], '.-')
+plt.plot(k[int(n/2):], fft_mag[int(n/2):], '.-' )
+plt.title('FFT')
+plt.show()
 
 #Parseval's theorem
 fsqravg = np.mean(np.abs(y) ** 2)
