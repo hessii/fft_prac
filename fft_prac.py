d221e887cb4a802438bbd85aea7e0da2684ba7a6
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-02 15:07:28 +0900

    edited fft x values using fft.fftfreq

--- fft_prac.py
+++ fft_prac.py
@@ -7,16 +7,17 @@ import matplotlib.pyplot as plt
 
 # Setup for domain
 # number of points
-n = 1000
+n = 500
 
 # Distance (in meters) or time period (in seconds)
-Lx = 100        # total period = 100 (total length of the graph)
+L = 100        # total period = 100 (total length of the graph)
 
 # angular frequency omega
-omg = 2.0 * np.pi / Lx
+omg = 2.0 * np.pi / L
 
 # Creating individual signals
-x = np.linspace(0, Lx, n)       # [0, 0.1001001, 0,2002002, ..., 100]
+x = np.linspace(0, L, n+1)       # [0, 0.1001001, 0,2002002, ..., 100]
+x = x[:n]
 y1 = 1. * np.cos(5. * omg * x)      # signal 1, angular velocity = 5w (5 times the angular velocity omg)
 y2 = 2. * np.sin(10. * omg * x)     # signal 2
 y3 = 0.5 * np.sin(20. * omg * x)    # signal 3
@@ -27,40 +28,38 @@ y = y1 + y2 + y3
 
 # Preparatory steps
 # Create all the necessary frequencies
-#fft.fftfreq(n, d=1.0): DFT sample frequencies, n: window length, d: sample spacing(default=1)
-dk = 2 * np.pi / Lx     # frequency spacing
-freqs = np.fft.fftfreq(int(dk*n), dk)      # [0,0.01, 0.02, ..., 4.99]
-
-# make array to be used for power spectra
-# ignoring half the values, as they are complex conjugates of the other
-# (ignoring negative frequencies)
-mask = freqs > 0
+#fft.fftfreq(n, d=1.0): DFT sample frequencies, n: # of samples, d: sample spacing(dx)(default=1)
+dx = L / n
+dk = 2 * np.pi / L     # frequency spacing
+freqs = np.fft.fftshift(np.fft.fftfreq(n, dx)) #[-0.5, -0.49, ..., -0.01, 0, 0.01, ..., 0.49]
 
 # FFT and power spectra calculations
 # fft values
 fft_vals = np.fft.fft(y)
+fft = np.fft.fftshift(fft_vals)
 
 # true theoretical fft
-# 2*|| for ..?
-fft_theo = 2.0 * np.abs(fft_vals) / n
+# 2*||/n for normalization
+fft_theo = 2.0 * np.abs(fft) / n
 
-plt.figure(1)
+plt.figure()
 plt.title('Original Signal')
 plt.plot(x, y, label='original')
 plt.legend()
 
-plt.figure(2)
-plt.plot(freqs, fft_vals, label='raw fft values')
-plt.title('Raw FFT values - need more processing')
+plt.figure()
+plt.plot(freqs, fft, '.-', label='raw fft values')
+plt.title('Raw FFT values')
 
-plt.figure(3)
-plt.plot(freqs[mask], fft_theo[mask], label='true fft values')
+plt.figure()
+plt.plot(freqs[int(n/2):], fft_theo[int(n/2):], '.-', label='true fft values')
 plt.title('True FFT values')
 
 plt.show()
 
 
-
+# want to extract the magnitudes and the x values for the peaks
+#
 
 
 
